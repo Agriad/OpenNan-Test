c532af49f936a435ce5e308b38687e60e196bdf8
Justin Arieltan <agriad1@yahoo.com>
2022-02-22 11:04:09 +0100

    add wifi aware support check
    step 2 of setup

--- app/src/main/java/com/example/opennan_test/MainActivity.kt
+++ app/src/main/java/com/example/opennan_test/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.opennan_test
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,13 +12,16 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.RequiresApi
 import com.example.opennan_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private var wifiAwareSupport = false
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,6 +37,13 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+
+        checkWifiAwareSupport(this)
+
+        if (wifiAwareSupport) {
+            Snackbar.make(binding.root, "Wifi Aware is supported", Snackbar.LENGTH_SHORT)
+                .show()
         }
     }
 
@@ -54,5 +67,11 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun checkWifiAwareSupport(context: Context) {
+        wifiAwareSupport =
+            context.packageManager.hasSystemFeature(PackageManager.FEATURE_WIFI_AWARE)
     }
 }
